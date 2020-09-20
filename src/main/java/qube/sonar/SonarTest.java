281e873834520a217d79b3847b6c3172e9bce425
anuvalluri27 <71574830+anuvalluri27@users.noreply.github.com>
2020-09-19 21:54:39 -0400

    initial commit

---
+++ src/main/java/qube/sonar/SonarTest.java
@@ -0,0 +1,27 @@
+package qube.sonar;
+
+import java.util.List;
+
+public class SonarTest {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	class Sonar {
+		private String test = "";
+		Sonar(){
+			
+			
+		}
+		
+		public void dummy(int number, List<String> names) {
+			if (number == 0) {
+				System.out.println("zero");
+			}
+			
+		}
+				
+	}
+
+}
